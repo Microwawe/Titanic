4bacf9c31bbe6ba4a75cea52f69e11c7d977cc52
Microwawe <microvvavve@gmail.com>
2017-07-14 21:17:03 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,9 @@
 <?php
-	$con = mysqli_connect("mysql.metropolia.fi","mirkon","O1BXHQFfaR7t3ZTH","mirkon");
+	$con = mysqli_connect("31.217.196.244","uogve_v","fa6Xaicu","uogvefke_titanic");
 
 	if (mysqli_connect_errno()) {
 	    echo "Failed to connect to MySQL: (" . mysqli_connect_errno() . ") " . mysqli_connect_error();
 	}
 
 	mysqli_set_charset($con, "utf8");
-
 
