cd4f9892130255ec3d918bf4151e39715226b66e
Mirko Nowak <microvvavve@gmail.com>
2017-08-21 21:36:50 +0300

    Updated database credentials

--- database.php
+++ database.php
@@ -1,9 +1,8 @@
 <?php
-	$con = mysqli_connect("31.217.196.244","uogve_v","fa6Xaicu","uogvefke_titanic");
+	$con = mysqli_connect("31.217.196.244","micro_titanic_admin","fa6Xaicu","micro_titanic");
 
 	if (mysqli_connect_errno()) {
 	    echo "Failed to connect to MySQL: (" . mysqli_connect_errno() . ") " . mysqli_connect_error();
 	}
 
 	mysqli_set_charset($con, "utf8");
-
