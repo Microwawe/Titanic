1fe70f94c2f6cf51df3bed54b53cc1699d9695b8
Mirko Nowak <microvvavve@gmail.com>
2018-02-01 13:01:05 +0200

    Minor update to html & css

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect("31.217.196.244","micro_titanic_admin","fa6Xaicu","micro_titanic");
+	$con = mysqli_connect();
 
 	if (mysqli_connect_errno()) {
 	    echo "Failed to connect to MySQL: (" . mysqli_connect_errno() . ") " . mysqli_connect_error();
